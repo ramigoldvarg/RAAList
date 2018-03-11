5d56bc2c486b8683cdb238bff71994515bdcc969
yonikliger <yonikliger@gmail.com>
2018-03-11 21:33:16 +0200

    fixed index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var path = require('path');
+
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
