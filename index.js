707c43c0f4e9f21155b005b3ebab89e654ebaa69
Nadav Halfon <nadav.halfon@gmail.com>
2018-03-12 01:37:26 +0200

    adding firebase

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  // Check local storage
-  res.render('login.html');
-});
+// app.get('/', function (req, res) {
+//   // Check local storage
+//   //res.render('login.html');
+// });
 
 // app.get('/home', function (req, res) {
 //   res.render("index.html");
